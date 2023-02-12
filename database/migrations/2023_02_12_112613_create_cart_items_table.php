80948e1bd98ace6d6f05f1536d9cbf7e7256022c
bashirahmed-cse <bashirahmed.cse@gmail.com>
2023-02-12 18:54:32 +0600

    first commit

---
+++ database/migrations/2023_02_12_112613_create_cart_items_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cart_items', function (Blueprint $table) {
+            $table->id();
+            $table->foreignIdFor(model: User::class, column: 'user_id');
+            $table->foreignId(column: 'product_id')->references(column: 'id')->on(table: 'products');
+            $table->integer(column: 'quantity');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cart_items');
+    }
+};
---
+++ database/migrations/2023_02_12_112835_create_customers_table.php
@@ -0,0 +1,37 @@
+<?php
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->id();
+            $table->string(column:'first_name');
+            $table->string(column:'last_name');
+            $table->string(column:'phone');
+            $table->string(column:'status', length:45)->nullable();
+            $table->timestamps();
+            $table->foreignIdFor(model: User::class, column:'created_by')->nullable();
+            $table->foreignIdFor(model: User::class, column:'updated_by')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customers');
+    }
+};
